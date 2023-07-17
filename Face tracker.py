da635ef6b01d16f80d171cb85e4a35d933a903d2
Scorpy-cmd <scorpyvvv@gmail.com>
2023-07-17 14:32:41 +0300

    split code into classes

--- Face tracker.py
+++ Face tracker.py
@@ -3,22 +3,59 @@ import cv2
 #import pygame
 #import numpy as np
 #import time
+import logging
+
+class DroneTracker:
+    def __init__(self, drone):
+        self.drone = drone
+        self.drone.streamon()
+
+        self.pid = [0.6, 0.4]
+        self.previous_correction = [0, 0, 0]
+        
+    def trackTarget(self, info, w, h, required_area):
+        area = info[1]
+        x, y = info[0]
+
+        yaw_correction = x - w // 2
+        yaw_speed = self.pid[0] * yaw_correction + self.pid[1] * (yaw_correction - self.previous_correction[0])
+        yaw_speed = int(self.clamp(yaw_speed, -100, 100))
+
+        front_correction = (required_area - area) // 70
+        front_speed = self.pid[0] * front_correction + self.pid[1] * (front_correction - self.previous_correction[1])
+        front_speed = int(self.clamp(front_speed, -100, 100))
+
+        up_correction = h // 2 - y
+        up_speed = self.pid[0] * up_correction - self.pid[1] * (up_correction - self.previous_correction[2])
+        up_speed = int(self.clamp(up_speed, -100, 100))
+
+        if area == 0:
+            yaw_speed = 0
+            front_speed = 0
+            up_speed = 0
+
+        self.drone.send_rc_control(0, front_speed, up_speed, yaw_speed)
+        self.previous_correction =  [yaw_correction, front_correction, up_correction]
+
+    def print_battery_state(self):
+        text = "Battery: {}%".format(self.drone.get_battery())
+        print(text)
+
+    def clamp(self, value, min_val, max_val):
+        return min(max(value, min_val), max_val)
+
+
+Tello.LOGGER.setLevel(logging.WARN)
+
+screen_w, screen_h = 360, 240
+required_area = 5000
 
 tello = Tello()
 tello.connect()
-tello.streamon()
-print(tello.get_battery())
-
 tello.takeoff()
 
-w, h = 360, 240
-required_area = 5000
-pid = [0.6, 0.4]
-pError = [0, 0, 0]
-
-def clamp(value, min_val, max_val):
-    return min(max(value, min_val), max_val)
-
+tracker = DroneTracker(tello)
+tracker.print_battery_state()
 
 def findFace(img):
     faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
@@ -37,7 +74,9 @@ def findFace(img):
         myFaceListArea.append(area)
 
     font = cv2.FONT_HERSHEY_SIMPLEX
-    cv2.putText(img, "Battery: {}%".format(tello.get_battery()), (2, 236), font, 0.3,(255,255,255), 1, cv2.LINE_AA)
+    # cv2.putText(img, "Battery: {}%".format(self.drone.get_battery()), (2, 236), font, 0.3, (255,255,255), 1, cv2.LINE_AA)
+    cv2.putText(img, "Battery: {}%".format(tello.get_battery()), (2, 236), font, 1, (255,255,255), 2, cv2.LINE_AA)
+    cv2.putText(img, "Remperature: {}".format(tello.get_temperature()), (2, 200), font, 1, (255,255,255), 2, cv2.LINE_AA)
 
     img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
 
@@ -48,36 +87,12 @@ def findFace(img):
         return img,[[0,0],0]
 
 
-def trackFace(info, w, pid, pError):
-    area = info[1]
-    x, y = info[0]
-
-    yaw_error = x - w // 2
-    yaw_speed = pid[0] * yaw_error + pid[1] * (yaw_error - pError[0])
-    yaw_speed = int(clamp(yaw_speed, -100, 100))
-
-    front_error = (required_area - area) // 70
-    front_speed = pid[0] * front_error + pid[1] * (front_error - pError[1])
-    front_speed = int(clamp(front_speed, -100, 100))
-
-    up_error = h // 2 - y
-    up_speed = pid[0] * up_error - pid[1] * (up_error - pError[2])
-    up_speed = int(clamp(up_speed, -100, 100))
-
-    if area == 0:
-        yaw_speed = 0
-        front_speed = 0
-        up_speed = 0
-
-    tello.send_rc_control(0, front_speed, up_speed, yaw_speed)
-    return [yaw_error, front_error, up_error]
-
-
 while True:
     img = tello.get_frame_read().frame
-    img = cv2.resize(img, (w,h))
+    img = cv2.resize(img, (screen_w, screen_h))
     img, info = findFace(img)
-    pError = trackFace(info, w , pid, pError)
 
     cv2.imshow("output", img)
     cv2.waitKey(1)
+
+    tracker.trackTarget(info, screen_w, screen_h, required_area)
