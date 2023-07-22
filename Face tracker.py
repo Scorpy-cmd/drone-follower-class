c083cf7c987570ef712e7f8b70c52a031be7f470
Scorpy-cmd <scorpyvvv@gmail.com>
2023-07-22 18:39:15 +0300

    updated following logics (made it 2-dismentional)

--- Face tracker.py
+++ Face tracker.py
@@ -1,4 +1,4 @@
-from djitellopy import Tello, TelloSwarm
+from djitellopy import Tello #, TelloSwarm
 import cv2
 #import pygame
 #import numpy as np
@@ -20,37 +20,31 @@ class DroneTracker:
         self.drone.streamon()
 
         self.pid = [0.6, 0.4]
-        self.previous_correction = AttributeDict({"yaw" : 0, "front" : 0, "up" : 0})
+        self.previous_correction = AttributeDict({"yaw" : 0, "front" : 0})
         self.has_tracked = False
         self.loss_timestamp = 0
         self.time_to_find = 4
 
         self.is_searching = False
         
-    def trackTarget(self, info, w, h, required_area):
-        area = info[1]
-        x, y = info[0]
+    def trackTarget(self, info, w, h):
+        x, y = info
 
         if self.is_searching:
-            self.searchForTarget(area)
+            self.searchForTarget(x)
             return
 
-        if area == 0 and not(self.has_tracked):
+        if x < 0 and not(self.has_tracked):
             return
         
         self.has_tracked = True
 
         yaw_correction = x - w // 2
-        front_correction = (required_area - area) // 70
-        up_correction = h // 2 - y
-        
-        if area == 0:
+        front_correction = h // 2 - y
+
+        if x < 0:
             yaw_correction = self.previous_correction.yaw * 0.99
-            up_correction = self.previous_correction.up
-            if (abs(self.previous_correction.up) < 20):
-                front_correction = self.previous_correction.front
-            else: 
-                front_correction = 0
+            front_correction = self.previous_correction.front
 
             if self.loss_timestamp == 0:
                 self.loss_timestamp = time.time() + self.time_to_find
@@ -69,21 +63,18 @@ class DroneTracker:
         front_speed = self.pid[0] * front_correction + self.pid[1] * (front_correction - self.previous_correction.front)
         front_speed = int(self.clamp(front_speed, -100, 100))
 
-        up_speed = self.pid[0] * up_correction - self.pid[1] * (up_correction - self.previous_correction.up)
-        up_speed = int(self.clamp(up_speed, -100, 100))
-
-        self.drone.send_rc_control(0, front_speed, up_speed, yaw_speed)
+        self.drone.send_rc_control(0, front_speed, 0, yaw_speed)
 
         self.previous_correction.yaw   = yaw_correction
-        self.previous_correction.up    = up_correction
         self.previous_correction.front = front_correction
 
     def findTarget(self):
         self.loss_timestamp = time.time() + self.time_to_find * 2
         self.is_searching = True
 
-    def searchForTarget(self, area):
-        if area != 0:
+    def searchForTarget(self, x):
+        if x >= 0:
+            print("Found target!")
             self.is_searching = False
             self.loss_timestamp = 0
             self.drone.send_rc_control(0, 0, 0, 0)
@@ -99,10 +90,9 @@ class DroneTracker:
             self.drone.send_rc_control(0, 0, 0, 0)
             print("Failed to find the target!")
 
-
     def reset(self):
         self.loss_timestamp = 0
-        self.previous_correction = AttributeDict({"yaw" : 0, "front" : 0, "up" : 0})
+        self.previous_correction = AttributeDict({"yaw" : 0, "front" : 0})
         self.has_tracked = False
         self.drone.send_rc_control(0, 0, 0, 0)
 
@@ -119,45 +109,40 @@ def findFace(img):
     imgGray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     faces = faceCascade.detectMultiScale(imgGray, 1.2, 8)
     myFaceListC = []
-    myFaceListArea = []
+    #myFaceListArea = []
 
     for (x, y, w, h) in faces:
         cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
         cx = x + w // 2
-        cy = y + h // 2
-        area = w * h
+        cy = y
         cv2.circle(img, (cx, cy), 5, (0, 255, 0), cv2.FILLED)
         myFaceListC.append([cx,cy])
-        myFaceListArea.append(area)
 
     font = cv2.FONT_HERSHEY_SIMPLEX
-    # cv2.putText(img, "Battery: {}%".format(self.drone.get_battery()), (2, 236), font, 0.3, (255,255,255), 1, cv2.LINE_AA)
+
     cv2.putText(img, "Battery: {}%".format(tello.get_battery()), (2, 236), font, 1, (255,255,255), 2, cv2.LINE_AA)
     cv2.putText(img, "Temperature: {}".format(tello.get_temperature()), (2, 200), font, 1, (255,255,255), 2, cv2.LINE_AA)
 
     img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
 
-    if len(myFaceListArea) != 0:
-        i = myFaceListArea.index(max(myFaceListArea))
-        return img, [myFaceListC[i], myFaceListArea[i]]
+    if len(myFaceListC) != 0:
+        return img, myFaceListC[0]
     else:
-        return img, [[0,0],0]
+        return img, [-1,0]
 
-
-#Tello.LOGGER.setLevel(logging.WARN)
+Tello.LOGGER.setLevel(logging.WARN)
 
 screen_w, screen_h = 360, 240
-required_area = 5000
 
 tello = Tello()
 tello.connect()
 tello.takeoff()
+tello.move_up(30)
 
 tracker = DroneTracker(tello)
 tracker.print_battery_state()
 
 timestamp = 0
-
 
 while True:
     if keyboard.is_pressed("q"):
@@ -170,6 +155,12 @@ while True:
         if not tracker.is_searching:
             tracker.findTarget()
 
+    if keyboard.is_pressed("w"):
+        tello.move_up(20)
+
+    if keyboard.is_pressed("s"):
+        tello.move_down(20)
+
     img = tello.get_frame_read().frame
     img = cv2.resize(img, (screen_w, screen_h))
     img, info = findFace(img)
@@ -177,6 +168,7 @@ while True:
     cv2.imshow("output", img)
     cv2.waitKey(1)
 
-    tracker.trackTarget(info, screen_w, screen_h, required_area)
+    tracker.trackTarget(info, screen_w, screen_h)
+
 else:
     print("Произошла ебтвоюмать")
