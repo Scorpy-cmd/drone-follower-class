c8aaaf590f9d32b2177a941a2ba79c4015d3a9f5
Scorpy-cmd <scorpyvvv@gmail.com>
2023-07-22 20:30:38 +0300

    dangerous squared movement
    there is too many sensitivity when going forward and backwards

--- Face tracker.py
+++ Face tracker.py
@@ -19,80 +19,52 @@ class DroneTracker:
         self.drone = drone
         self.drone.streamon()
 
-        self.pid = [0.6, 0.4]
-        self.previous_correction = AttributeDict({"yaw" : 0, "front" : 0})
+        self.pid = [0.3, 0.4]
+        self.previous_correction = AttributeDict({"side" : 0, "front" : 0})
         self.has_tracked = False
         self.loss_timestamp = 0
         self.time_to_find = 4
-
-        self.is_searching = False
         
     def trackTarget(self, info, w, h):
         x, y = info
-
-        if self.is_searching:
-            self.searchForTarget(x)
-            return
 
         if x < 0 and not(self.has_tracked):
             return
         
         self.has_tracked = True
 
-        yaw_correction = x - w // 2
-        front_correction = h // 2 - y
+        side_correction = x - w // 2
+        front_correction = y - h // 2
 
         if x < 0:
-            yaw_correction = self.previous_correction.yaw * 0.99
+            side_correction = self.previous_correction.side
             front_correction = self.previous_correction.front
 
             if self.loss_timestamp == 0:
                 self.loss_timestamp = time.time() + self.time_to_find
 
             if self.loss_timestamp < time.time():
-                print(self.loss_timestamp)
+                #print(self.loss_timestamp)
                 print("LOST THE TARGET!!")
                 self.reset()
                 return
         else:
             self.loss_timestamp = 0
 
-        yaw_speed = self.pid[0] * yaw_correction + self.pid[1] * (yaw_correction - self.previous_correction.yaw)
-        yaw_speed = int(self.clamp(yaw_speed, -100, 100))
+        side_speed = self.pid[0] * side_correction + self.pid[1] * (side_correction - self.previous_correction.side)
+        side_speed = int(self.clamp(side_speed, -100, 100))
 
         front_speed = self.pid[0] * front_correction + self.pid[1] * (front_correction - self.previous_correction.front)
-        front_speed = int(self.clamp(front_speed, -100, 100))
+        front_speed = - int(self.clamp(4 * front_speed, -100, 100))
 
-        self.drone.send_rc_control(0, front_speed, 0, yaw_speed)
+        self.drone.send_rc_control(side_speed, front_speed, 0, 0)
 
-        self.previous_correction.yaw   = yaw_correction
+        self.previous_correction.side  = side_correction
         self.previous_correction.front = front_correction
-
-    def findTarget(self):
-        self.loss_timestamp = time.time() + self.time_to_find * 2
-        self.is_searching = True
-
-    def searchForTarget(self, x):
-        if x >= 0:
-            print("Found target!")
-            self.is_searching = False
-            self.loss_timestamp = 0
-            self.drone.send_rc_control(0, 0, 0, 0)
-            return
-
-        if self.loss_timestamp > time.time():
-            self.drone.send_rc_control(30, 0, 30, -60)
-        elif self.loss_timestamp + self.time_to_find * 2 > time.time():
-            self.drone.send_rc_control(0, 0, -30, 0)
-        else:
-            self.is_searching = False
-            self.loss_timestamp = 0
-            self.drone.send_rc_control(0, 0, 0, 0)
-            print("Failed to find the target!")
 
     def reset(self):
         self.loss_timestamp = 0
-        self.previous_correction = AttributeDict({"yaw" : 0, "front" : 0})
+        self.previous_correction = AttributeDict({"side" : 0, "front" : 0})
         self.has_tracked = False
         self.drone.send_rc_control(0, 0, 0, 0)
 
@@ -114,7 +86,7 @@ def findFace(img):
     for (x, y, w, h) in faces:
         cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
         cx = x + w // 2
-        cy = y
+        cy = y #- h // 2
         cv2.circle(img, (cx, cy), 5, (0, 255, 0), cv2.FILLED)
         myFaceListC.append([cx,cy])
 
@@ -137,7 +109,7 @@ screen_w, screen_h = 360, 240
 tello = Tello()
 tello.connect()
 tello.takeoff()
-tello.move_up(30)
+tello.move_up(40)
 
 tracker = DroneTracker(tello)
 tracker.print_battery_state()
@@ -149,11 +121,8 @@ while True:
         tello.send_rc_control(0, 0, 0, 0)
         tello.land()
         tello.streamoff()
+        cv2.destroyAllWindows()
         break
-
-    if keyboard.is_pressed("r"):
-        if not tracker.is_searching:
-            tracker.findTarget()
 
     if keyboard.is_pressed("w"):
         tello.move_up(20)
@@ -167,6 +136,7 @@ while True:
 
     cv2.imshow("output", img)
     cv2.waitKey(1)
+
 
     tracker.trackTarget(info, screen_w, screen_h)
 
