1ea6cda707bee9044c8c79c399f582fe8b580a28
Dmitry Bush <bushuev0458@gmail.com>
2023-07-24 23:40:37 +0300

    Start screen until all drones will initialized

--- source.py
+++ source.py
@@ -2,6 +2,7 @@ import multiprocessing
 # import pygame
 from script import script
 from time import sleep, time
+from button import Menu
 import keyboard
 
 
@@ -12,20 +13,32 @@ drones = []
 "192.168.0.136"
 drones_ips = ['192.168.0.113', '192.168.0.118']
 print(">>> statring the process")
-def main():
+
+
+def main(condition):
     print(">>> setting up the variables")
     arr = multiprocessing.Array('b', [False] * drones_count)
     init_check = multiprocessing.Array('b', [False] * (drones_count))
     running = multiprocessing.Value('b', True)
     print(">>> setting up processes")
     for i in range(drones_count):
-        drones.append(multiprocessing.Process(target=script, args=(drones_ips[i], 17600 + i * 11, 8080 + i * 7, i, arr, init_check, running)))
+        drones.append(multiprocessing.Process(target=script, args=(
+        drones_ips[i], 17600 + i * 11, 8080 + i * 7, i, arr, init_check, running)))
         drones[i].start()
         sleep(4)
 
+    condition[0] = False
     print("joining")
     for i in range(drones_count):
-       drones[i].join()
+        drones[i].join()
+
 
 if __name__ == "__main__":
-    main()
+    condition = multiprocessing.Array('b', [False])
+    start_process = multiprocessing.Process(target=Menu, args=(condition,))
+    start_process.start()
+
+    while True:
+        if condition[0]:
+            break
+    main(condition)
