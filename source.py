da546a3fc5427dd082e35964155b6875f78796b0
Dmitry Bush <bushuev0458@gmail.com>
2023-07-24 23:50:34 +0300

    Start screen closes after drone movement

--- source.py
+++ source.py
@@ -15,7 +15,7 @@ drones_ips = ['192.168.0.113', '192.168.0.118']
 print(">>> statring the process")
 
 
-def main(condition):
+def main(loading_condition):
     print(">>> setting up the variables")
     arr = multiprocessing.Array('b', [False] * drones_count)
     init_check = multiprocessing.Array('b', [False] * (drones_count))
@@ -23,22 +23,21 @@ def main(condition):
     print(">>> setting up processes")
     for i in range(drones_count):
         drones.append(multiprocessing.Process(target=script, args=(
-        drones_ips[i], 17600 + i * 11, 8080 + i * 7, i, arr, init_check, running)))
+            drones_ips[i], 17600 + i * 11, 8080 + i * 7, i, arr, init_check, running, loading_condition)))
         drones[i].start()
         sleep(4)
 
-    condition[0] = False
     print("joining")
     for i in range(drones_count):
         drones[i].join()
 
 
 if __name__ == "__main__":
-    condition = multiprocessing.Array('b', [False])
-    start_process = multiprocessing.Process(target=Menu, args=(condition,))
+    loading_condition = multiprocessing.Array('b', [False])
+    start_process = multiprocessing.Process(target=Menu, args=(loading_condition,))
     start_process.start()
 
     while True:
-        if condition[0]:
+        if loading_condition[0]:
             break
-    main(condition)
+    main(loading_condition)
